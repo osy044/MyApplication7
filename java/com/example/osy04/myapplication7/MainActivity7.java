c2341f36d885a1b490f277db681d534d32c1311a
Seung <osy044@naver.com>
2017-10-07 02:43:06 +0900

    lalala2

--- java/com/example/osy04/myapplication7/MainActivity7.java
+++ java/com/example/osy04/myapplication7/MainActivity7.java
@@ -12,5 +12,6 @@ public class MainActivity7 extends AppCompatActivity {
 
 //        testestsetestestsetsetetestestse
         // lalsalalalalal
+        onCreateSupportNavigateUpTaskStack();
     }
 }
