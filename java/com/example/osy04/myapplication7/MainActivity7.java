14f1c5068a814806f99195c7ef51c74f56368791
Seung <osy044@naver.com>
2017-10-07 02:41:07 +0900

    first test

---
+++ java/com/example/osy04/myapplication7/MainActivity7.java
@@ -0,0 +1,13 @@
+package com.example.osy04.myapplication7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+}
